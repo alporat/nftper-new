9376ff23cdecbcad2bfa86c0dfcf3027be394707
alporat <53021579+alporat@users.noreply.github.com>
2024-12-23 02:31:11 -0500

    Update logo.tsx

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -3,9 +3,9 @@ import { Layers } from 'lucide-react';
 export function Logo() {
   return (
     <div className="flex items-start p-1">
-      <Layers className="w-8 h-8 text-green-500 mr-2" />
+      <Layers className="w-8 h-8 text-main-green mr-2" />
       <span className="text-xl font-bold">
-        NFT<span className="text-green-500">PER</span>
+        NFT<span className="text-main-green">PER</span>
       </span>
     </div>
   );
